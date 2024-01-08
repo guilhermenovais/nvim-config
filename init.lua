1cdc9c596521c3936844882a47aee351f6b241ca
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-01-08 09:46:24 -0300

    feat: adding commands to which_key

--- init.lua
+++ init.lua
@@ -511,12 +511,14 @@ end
 -- document existing key chains
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>t'] = { desc = 'Toggle Tree' },
+  ['<leader>d'] = { desc = 'Toggle Diffview' },
+  ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' }
 }
 
 -- mason-lspconfig requires that these setup functions are called in this order
