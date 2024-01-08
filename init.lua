6b22276c94036d0a9c5c59296188de090e386b43
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-01-08 08:14:08 -0300

    feat: configuring trouble.nvim

--- init.lua
+++ init.lua
@@ -245,6 +245,12 @@ require('lazy').setup({
   },
   {
     'sindrets/diffview.nvim'
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+    }
   }
 }, {})
 
@@ -638,6 +644,14 @@ function DiffviewToggle()
   end
 end
 vim.keymap.set('n', '<leader>d', DiffviewToggle, { desc = 'Toggle Diffview' })
+
+-- Trouble setup
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
