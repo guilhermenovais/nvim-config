c474e976fa5ed56686620f4d094d8284c28a8200
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2024-03-31 13:05:12 -0300

    feat: changing autocomplete behaviour to insert

--- init.lua
+++ init.lua
@@ -607,7 +607,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.insert,
       select = true,
     },
     ['<Tab>'] = nil,
